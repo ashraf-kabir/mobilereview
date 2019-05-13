8ac90f2dfe67f9ae5839ee43008e1e31e60283ec
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-05-13 20:58:36 +0600

    changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,8 @@
-<footer class="py-5 bg-dark">
-    <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Mobile Review MMXIX</p>
-    </div>
-    <!-- /.container -->
-</footer>
+<div class="fixed-bottom">
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Mobile Review MMXIX</p>
+        </div>
+        <!-- /.container -->
+    </footer>
+</div>
